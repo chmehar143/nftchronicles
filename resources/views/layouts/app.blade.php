aa12ab0c7ff870ee2f67f12d92d5fb7ac35d9b18
chmehar143 <chmehar143@gmail.com>
2022-04-06 01:18:00 +0500

    frontend website

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+
+
+
+
+
+<!-- Scroll To Top -->
+<div id="scrollTopButton"><i class="bi bi-arrow-up-short"></i></div>
+<!-- All JavaScript Files-->
+<script src="{{ asset('js/all-js-libraries.js')}}"></script>
+<script src="{{ asset('js/active.js')}}"></script>
+<script src="{{ asset('js/charts.js')}}"></script>
+<script>
+    function openSearch() {
+        document.getElementById("myOverlay").style.display = "block";
+    }
+
+    function closeSearch() {
+        document.getElementById("myOverlay").style.display = "none";
+    }
+</script>
+<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<!-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
+</body>
+
+</html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,68 @@
+<!-- Footer -->
+<footer class="footer-area pb-120 pt-120" style="background-image: url('img/bg-img/1.jpg')"><img class="footer-bg-shape" src="img/core-img/shape1.png" alt="" data-aos="fade-up" data-aos-duration="500" data-aos-delay="200">
+    <div class="container">
+        <div class="row">
+            <!-- Footer Widget -->
+            <div class="col-12 col-lg-5">
+                <div class="footer-widget-area mb-70 pe-lg-4 pe-xl-5 me-lg-4 me-xl-5 border-end"><a class="d-block mb-4" href="index.html"><img class="light-logo" src="img/core-img/logo.png" alt=""><img class="dark-logo" src="img/core-img/logo-white.png" alt=""></a>
+                    <p>It's crafted with the latest trend of design &amp; coded with all modern approaches.</p>
+                    <p class="mb-0">Call: +123 456 789 <br> Email: help@example.com</p>
+                    <!-- Social Icon -->
+                    <h5 class="mt-4 mb-3">Join the community</h5>
+                    <div class="footer-social-icon d-flex align-items-center flex-wrap"><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Facbook"><img src="img/core-img/icons8-facebook.svg" alt=""></a><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Twitter"><img src="img/core-img/icons8-twitter.svg" alt=""></a><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Instagram"><img src="img/core-img/icons8-instagram.svg" alt=""></a><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Slack"><img src="img/core-img/icons8-slack.svg" alt=""></a><a href="#" data-bs-toggle="tooltip" data-bs-placement="top" title="Youtube"><img src="img/core-img/icons8-player.svg" alt=""></a></div>
+                    <!-- Newsletter Form -->
+                    <div class="newsletter-form mt-5 me-5">
+                        <form class="d-flex align-items-stretch" action="#">
+                            <input class="form-control" type="email" placeholder="Enter email">
+                            <button class="btn btn-warning px-3" type="submit">Subscribe</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- Footer Widget -->
+            <div class="col-12 col-lg-7">
+                <div class="row g-4">
+                    <div class="col-6 col-sm-6">
+                        <div class="footer-widget-area mb-70">
+                            <h5 class="mb-4">Discover</h5>
+                            <ul class="list-unstyled mb-0">
+                                <li><a href="all.html">All NFTs</a></li>
+                                <li><a href="today.html">Today NFTs</a></li>
+                                <li><a href="newest.html">Newest NFTs</a></li>
+                                <li><a href="upcoming.html">UpComing NFTs</a></li>
+                                <li><a href="ongoing.html">OnGoing NFTs</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-6 col-sm-6">
+                        <div class="footer-widget-area mb-70">
+                            <h5 class="mb-4">Company</h5>
+                            <ul class="list-unstyled mb-0">
+                                <li><a href="blog.html">News</a></li>
+                                <li><a href="help-center.html">Faqs</a></li>
+                                <li><a href="contact.html">Contact Us</a></li>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="copywrite-wrapper d-lg-flex align-items-lg-center justify-content-lg-between">
+            <!-- Copywrite Text -->
+            <div class="copywrite-text text-center text-lg-start mb-3 mb-lg-0">
+                <p class="mb-0">2022 &copy; All rights reserved by <a href="#" target="_blank" rel="noreferrer">BS3 Designs</a></p>
+            </div>
+            <!-- Footer Nav -->
+            <div class="footer-nav">
+                <ul class="mb-0 d-flex flex-wrap justify-content-center list-unstyled">
+                    <!-- <li><a href="privacy.html">Privacy Policy</a></li>
+                    <li><a href="terms.html">Terms &amp; Conditions</a></li> -->
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
