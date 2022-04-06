1242fbe0d55f710c8e1b251934585bbcc7bfef88
chmehar143 <chmehar143@gmail.com>
2022-04-06 20:00:28 +0500

    update css

--- public/js/active.js
+++ public/js/active.js
@@ -547,3 +547,10 @@ if (preloader) {
         }, 20);
     });
 }
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+  }
+  
+  function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+  }
