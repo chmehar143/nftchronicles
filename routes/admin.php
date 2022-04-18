e0b3b4150b8d56b0126637c654cbcd204479169d
chmehar143 <chmehar143@gmail.com>
2022-04-19 00:32:40 +0500

    "user create by admin"

--- routes/admin.php
+++ routes/admin.php
@@ -10,7 +10,7 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::get('/list', 'NftController@list')->name('nftlist');
 Route::get('/create', 'NftController@create')->name('nftcreate');
 Route::get('/edit/{nft}', 'NftController@edit')->name('nftedit');
-Route::get('/{nft}', 'NftController@view')->name('nftview');
+Route::get('/view/{nft}', 'NftController@view')->name('nftview');
 Route::post('/save', 'NftController@save')->name('nftsave');
 Route::get('/delete/{id}', 'NftController@destroy')->name('nftdestroy');
 
