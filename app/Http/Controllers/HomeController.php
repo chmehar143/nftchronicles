d6d0c5bef667fc81e2eed9ccb70afb2a381ce635
chmehar143 <chmehar143@gmail.com>
2022-06-02 01:33:51 +0500

    "add user and permission"

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,7 +20,7 @@ class HomeController extends Controller
       $snews = News::where('show_home_page', 1)->where('category', 'Simple News')->take(6)->get();
       $news = News::where('show_home_page', 1)->where('category', 'Popular Post')->take(5)->get();
       $nfts = Nfts::where('show_home_page', 1)->orWhere('show_feature_post', 1)->orderBy('pre_sale_date', 'DESC')->take(8)->get();
-      $sponnfts = Nfts::where('is_sponsored_post', 1)->orderBy('pre_sale_date', 'DESC')->take(5)->get();
+      $sponnfts = Nfts::where('is_sponsored_post', 1)->take(5)->get();
       return view('welcome',compact('advirtisements','banners','faqs','news','nfts','snews','sponnfts'));
   }
 }
