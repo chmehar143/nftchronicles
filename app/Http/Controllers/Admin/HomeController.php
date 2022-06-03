dc8c28415c75741340864111b8fc0a5f53effd65
usama.sarfraz@piecyfer.com <nusama143>
2022-06-03 13:33:11 +0500

    "chat added in record"

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Models\News;
 use App\Models\Nfts;
 use Illuminate\Support\Facades\Auth;
+use DB;
 
 class HomeController extends Controller
 {
@@ -30,7 +31,25 @@ class HomeController extends Controller
             $sponsored_post = Nfts::whereMonth('updated_at', date('m'))->where('is_sponsored_post','=','1')->count();
             $user_nft = Nfts::whereMonth('updated_at', date('m'))->where('insert_side','=','user')->count();
             $feature_post = Nfts::whereMonth('updated_at', date('m'))->where('show_feature_post','=','1')->count();
-            return view('admin.home',compact('sponsored_post','user_nft','feature_post'));
+            $month_wise_count = [];
+            $month = [];
+            $items = Nfts::select(
+                DB::raw("(COUNT(*)) as count"),
+                DB::raw("MONTHNAME(created_at) as month_name")
+            )
+                ->whereYear('created_at', date('Y'))
+                ->where('status', 1)
+                ->groupBy('month_name')
+                ->get()
+                ->toArray();
+            foreach ($items as $data){
+                $month_wise_count[] = $data['count'];
+                $month[] = $data['month_name'];
+            }
+            $sponsored_nft = Nfts::where('is_sponsored_post','=','1')->where('status', 1)->count();
+            $simple_nft = Nfts::where('is_sponsored_post','=','0')->where('show_feature_post','=','0')->where('status', 1)->count();
+            $feature_nft = Nfts::where('show_feature_post','=','1')->where('status', 1)->count();
+            return view('admin.home',compact('sponsored_post','user_nft','feature_post','month_wise_count','month','sponsored_nft','simple_nft','feature_nft'));
         }elseif ($role == 'admin')
         {
             $nfts = Nfts::get();
