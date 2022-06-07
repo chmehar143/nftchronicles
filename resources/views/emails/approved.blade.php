220079361c50d58d6933c8f302b3017eb341f165
usama.sarfraz@piecyfer.com <nusama143>
2022-06-07 16:14:23 +0500

    "record update accoding to email"

--- resources/views/emails/approved.blade.php
+++ resources/views/emails/approved.blade.php
@@ -226,7 +226,7 @@
 											<div class="card-body" style="margin-top:-1px;"><a
 													href="#">
 													<div class="img-wrap">
-                                                        <img src="https://www.nftchronicles.co.uk/img/1.gif" style="width: 99%;"
+                                                        <img src="{{$details->file_path}}" style="width: 99%;"
 															class="img-fluid" alt="">
 														<!-- Badge -->
                                                         @if($details->show_feature_post ==1)
