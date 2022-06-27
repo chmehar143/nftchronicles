d0362029b9d15fd414beeb144a1e8bb5d43389cb
Naveed1222 <johneswilliam28@gmail.com>
2022-06-27 23:01:51 +0500

    changes

---
+++ resources/views/About-us.blade.php
@@ -0,0 +1,274 @@
+@extends('layouts.app')
+
+@section('content')
+ <!-- About -->
+ <style>
+  .counter{
+font-size:58px !important;
+  }
+  .cards {
+    background-color:#DC6A2A !important;
+  }
+
+ </style>
+
+ <div style="background-color:#272727 !important">
+ <div class="about-area" style="background-color:#1F1F1F !important">
+ <br><br><Br><Br><Br><br><Br>
+      <div class="container">
+        <h1 class="text-center">Get the insight you need to thrive in the fast-paced world of NFTs</h1><br><br><br>
+        <div class="row g-4">
+          <div class="col-12 col-sm-6 col-xl-3">
+            <div class="card about-card shadow-sm">
+              <div class="card-body py-4">
+                <div class="icon bg-success"><i class="bi bi-shield-fill-check"></i></div>
+                <h4 class="mb-3">Trustworthy</h4>
+                <p class="mb-0">It's crafted with the latest trend of design &amp; coded with all modern approaches.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-xl-3">
+            <div class="card about-card shadow-sm">
+              <div class="card-body py-4">
+                <div class="icon bg-primary"><i class="bi bi-shield-lock"></i></div>
+                <h4 class="mb-3">Secure &amp; Safe</h4>
+                <p class="mb-0">It's crafted with the latest trend of design &amp; coded with all modern approaches.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-xl-3">
+            <div class="card about-card shadow-sm">
+              <div class="card-body py-4">
+                <div class="icon bg-info"><i class="bi bi-people-fill"></i></div>
+                <h4 class="mb-3">Loyal Customer</h4>
+                <p class="mb-0">It's crafted with the latest trend of design &amp; coded with all modern approaches.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-12 col-sm-6 col-xl-3">
+            <div class="card about-card shadow-sm">
+              <div class="card-body py-4">
+                <div class="icon bg-danger"><i class="bi bi-wallet2"></i></div>
+                <h4 class="mb-3">Easy Wallet</h4>
+                <p class="mb-0">It's crafted with the latest trend of design &amp; coded with all modern approaches.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div><br><Br><br>
+      <div class="divider"></div>
+      <div class="container">
+        <div class="row g-4 g-xl-5 align-items-center justify-content-between">
+          <div class="col-12 col-lg-7 col-xl-6">
+            <h2 class="h2 fw-bold mb-3">Digital arts are trends now. Welcome to the world of NFTs.</h2>
+            <p class="fz-18">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor quae non officiis itaque cum. Tenetur, doloremque hic, reiciendis fugit</p>
+            <p class="fz-18">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste magnam quod ratione ipsum provident itaque repellendus exercitationem, s</p>
+            <a href="" class="tf-button-st2 btn-effect" data-toggle=""><span class="effect">Know More </span></a>
+          </div>
+          <div class="col-12 col-lg-5"><img class="rounded" src="img/illustrator/4.png" alt=""></div>
+        </div>
+      </div>
+      <div class="divider"></div>
+      <!-- CTA Wrapper -->
+   
+      <div class="divider"></div>
+      <div class="container">
+        <div class="row g-4 g-xl-5 align-items-center justify-content-between">
+          <div class="col-12 col-lg-5"><img class="rounded" src="img/illustrator/4.png" alt=""></div>
+          <div class="col-12 col-lg-7 col-xl-6">
+            <h2 class="h2 fw-bold mb-3">Explore, buy, and sell exceptional NFTs.</h2>
+            <p class="fz-18">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor quae non officiis itaque cum. Tenetur, doloremque hic, reiciendis fugit</p>
+            <p class="fz-18">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste magnam quod ratione ipsum provident itaque repellendus exercitationem, s</p>
+            <a href="" class="tf-button-st2 btn-effect" data-toggle=""><span class="effect">Know More </span></a>          </div>
+        </div>
+<br><br><br><br><br>
+
+      </div>
+    </div>
+<br><br><br><br><br>
+    <div class="container" style="background-color:#1F1F1F !important">
+      <div class="counter-wrap p-5 shadow border">
+        <div class="row g-4">
+          <!-- Counter -->
+          <div class="col-6 col-md-3">
+            <div class="single-counter text-center">
+              <h4 class="counter h2 fw-bold">3409</h4>
+              <h6 class="mb-0">Total Items</h6>
+            </div>
+          </div>
+          <!-- Counter -->
+          <div class="col-6 col-md-3">
+            <div class="single-counter text-center">
+              <h4 class="counter h2 fw-bold">7831</h4>
+              <h6 class="mb-0">Users</h6>
+            </div>
+          </div>
+          <!-- Counter -->
+          <div class="col-6 col-md-3">
+            <div class="single-counter text-center">
+              <h4 class="counter h2 fw-bold">45236</h4>
+              <h6 class="mb-0">NFTs</h6>
+            </div>
+          </div>
+          <!-- Counter -->
+          <div class="col-6 col-md-3">
+            <div class="single-counter text-center">
+              <h4 class="counter h2 fw-bold">247</h4>
+              <h6 class="mb-0">Daily Sale</h6>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+<br><br><br><br><br>
+
+    <div class="divider"></div>
+    <!-- Client Feedback -->
+    <div class="client-feedback-wrap">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 col-sm-9 col-md-8 col-lg-7">
+            <div class="section-heading">
+              <h2>Read some reviews from our beloved clients about our work.</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container">
+        <div class="client-feedback-content">
+          <div class="client-feedback-slides">
+            <!-- Single Feedback  -->
+            <div>
+              <div class="cards feedback-card p-2">
+                <div class="card-body p-4">
+                  <div class="client-info d-flex align-items-center">
+                    <div class="client-thumb rounded-circle me-1 position-relative"><img class="rounded-circle" src="img/bg-img/u1.jpg" alt=""></div>
+                    <div class="client-name">
+                      <h6 class="fz-16 mb-0">Designing World</h6>
+                      <p class="mb-0 fz-14">@designing_world</p>
+                    </div>
+                  </div>
+                  <div class="ratings text-warning mt-4 mb-3 fz-14"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></div>
+                  <p class="text-dark mb-0 fw-bold">You've saved our business! Thanks guys, keep up the good work!</p>
+                </div>
+              </div>
+            </div>
+            <!-- Single Feedback Slide -->
+            <div>
+              <div class="cards feedback-card p-2">
+                <div class="card-body p-4">
+                  <div class="client-info d-flex align-items-center">
+                    <div class="client-thumb rounded-circle me-1 position-relative"><img class="rounded-circle" src="img/bg-img/u2.jpg" alt=""></div>
+                    <div class="client-name">
+                      <h6 class="fz-16 mb-0">Designing World</h6>
+                      <p class="mb-0 fz-14">@designing_world</p>
+                    </div>
+                  </div>
+                  <div class="ratings text-warning mt-4 mb-3 fz-14"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></div>
+                  <p class="text-dark mb-0 fw-bold">I strongly recommend to everyone interested in running a business!</p>
+                </div>
+              </div>
+            </div>
+            <!-- Single Feedback Slide -->
+            <div>
+              <div class="cards feedback-card p-2">
+                <div class="card-body p-4">
+                  <div class="client-info d-flex align-items-center">
+                    <div class="client-thumb rounded-circle me-1 position-relative"><img class="rounded-circle" src="img/bg-img/u3.jpg" alt=""></div>
+                    <div class="client-name">
+                      <h6 class="fz-16 mb-0">Designing World</h6>
+                      <p class="mb-0 fz-14">@designing_world</p>
+                    </div>
+                  </div>
+                  <div class="ratings text-warning mt-4 mb-3 fz-14"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></div>
+                  <p class="text-dark mb-0 fw-bold">Absolutely wonderful! I wish I would have thought of it first. Excellent product!</p>
+                </div>
+              </div>
+            </div>
+            <!-- Single Feedback  -->
+            <div>
+              <div class="cards feedback-card p-2">
+                <div class="card-body p-4">
+                  <div class="client-info d-flex align-items-center">
+                    <div class="client-thumb rounded-circle me-1 position-relative"><img class="rounded-circle" src="img/bg-img/u4.jpg" alt=""></div>
+                    <div class="client-name">
+                      <h6 class="fz-16 mb-0">Designing World</h6>
+                      <p class="mb-0 fz-14">@designing_world</p>
+                    </div>
+                  </div>
+                  <div class="ratings text-warning mt-4 mb-3 fz-14"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></div>
+                  <p class="text-dark mb-0 fw-bold">You've saved our business! Thanks guys, keep up the good work!</p>
+                </div>
+              </div>
+            </div>
+            <!-- Single Feedback  -->
+            <div>
+              <div class="cards feedback-card p-2">
+                <div class="card-body p-4">
+                  <div class="client-info d-flex align-items-center">
+                    <div class="client-thumb rounded-circle me-1 position-relative"><img class="rounded-circle" src="img/bg-img/u1.jpg" alt=""></div>
+                    <div class="client-name">
+                      <h6 class="fz-16 mb-0">Designing World</h6>
+                      <p class="mb-0 fz-14">@designing_world</p>
+                    </div>
+                  </div>
+                  <div class="ratings text-warning mt-4 mb-3 fz-14"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></div>
+                  <p class="text-dark mb-0 fw-bold">You've saved our business! Thanks guys, keep up the good work!</p>
+                </div>
+              </div>
+            </div>
+            <!-- Single Feedback Slide -->
+            <div>
+              <div class="cards feedback-card p-2">
+                <div class="card-body p-4">
+                  <div class="client-info d-flex align-items-center">
+                    <div class="client-thumb rounded-circle me-1 position-relative"><img class="rounded-circle" src="img/bg-img/u2.jpg" alt=""></div>
+                    <div class="client-name">
+                      <h6 class="fz-16 mb-0">Designing World</h6>
+                      <p class="mb-0 fz-14">@designing_world</p>
+                    </div>
+                  </div>
+                  <div class="ratings text-warning mt-4 mb-3 fz-14"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></div>
+                  <p class="text-dark mb-0 fw-bold">I strongly recommend to everyone interested in running a business!</p>
+                </div>
+              </div>
+            </div>
+            <!-- Single Feedback Slide -->
+            <div>
+              <div class="cards feedback-card p-2">
+                <div class="card-body p-4">
+                  <div class="client-info d-flex align-items-center">
+                    <div class="client-thumb rounded-circle me-1 position-relative"><img class="rounded-circle" src="img/bg-img/u3.jpg" alt=""></div>
+                    <div class="client-name">
+                      <h6 class="fz-16 mb-0">Designing World</h6>
+                      <p class="mb-0 fz-14">@designing_world</p>
+                    </div>
+                  </div>
+                  <div class="ratings text-warning mt-4 mb-3 fz-14"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></div>
+                  <p class="text-dark mb-0 fw-bold">Absolutely wonderful! I wish I would have thought of it first. Excellent product!</p>
+                </div>
+              </div>
+            </div>
+            <!-- Single Feedback  -->
+            <div>
+              <div class="cards feedback-card p-2">
+                <div class="card-body p-4">
+                  <div class="client-info d-flex align-items-center">
+                    <div class="client-thumb rounded-circle me-1 position-relative"><img class="rounded-circle" src="img/bg-img/u4.jpg" alt=""></div>
+                    <div class="client-name">
+                      <h6 class="fz-16 mb-0">Designing World</h6>
+                      <p class="mb-0 fz-14">@designing_world</p>
+                    </div>
+                  </div>
+                  <div class="ratings text-warning mt-4 mb-3 fz-14"><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i><i class="bi bi-star-fill"></i></div>
+                  <p class="text-dark mb-0 fw-bold">You've saved our business! Thanks guys, keep up the good work!</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br><br><br>
+</div>
+@endsection
